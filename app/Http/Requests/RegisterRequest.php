7afd5bbe6822f214034c855946a9481f229706b2
Rafael Miranda <r4free@gmail.com>
2022-05-13 00:14:40 -0300

    Classe de validação de cadastro

--- app/Http/Requests/RegisterRequest.php
+++ app/Http/Requests/RegisterRequest.php
@@ -13,7 +13,7 @@ class RegisterRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -24,7 +24,10 @@ class RegisterRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+           'school_name' => ['required'],
+           'email' => ['required'],
+           'name' => ['required'],
+           'password' => ['required'],
         ];
     }
 }
