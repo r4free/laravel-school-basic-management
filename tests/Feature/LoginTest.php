d001a384a0197982ce05f64537ef93924dc114f3
Rafael Miranda <r4free@gmail.com>
2022-05-13 10:59:29 -0300

    teste de login

--- tests/Feature/LoginTest.php
+++ tests/Feature/LoginTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Models\Manager;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -13,10 +14,26 @@ class LoginTest extends TestCase
      *
      * @return void
      */
+    use RefreshDatabase;
+    
     public function test_login_page()
     {
         $response = $this->get('/login');
 
         $response->assertStatus(200);
+    }
+
+    public function test_login()
+    {
+
+        $manager =  Manager::first();
+        
+        $response = $this->post(route('login'),[
+            'email' => $manager->email,
+            'password' => 'password'
+        ]);
+
+        $response->assertRedirect();
+
     }
 }
