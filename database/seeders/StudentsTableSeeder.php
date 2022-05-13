64d9463fe9655a67e76cda34f6a1398b09de681b
Rafael Miranda <r4free@gmail.com>
2022-05-13 11:01:47 -0300

    Redução numero de registro factory aluno

--- database/seeders/StudentsTableSeeder.php
+++ database/seeders/StudentsTableSeeder.php
@@ -17,7 +17,7 @@ class StudentsTableSeeder extends Seeder
     public function run()
     {
         School::get()->each(function($school){
-            Student::factory()->count(100)
+            Student::factory()->count(30)
             ->create([
                 'school_id' => $school->id
             ]);
