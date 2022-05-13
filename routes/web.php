b99fa8c893b49d4123bfddd92177c2279490f857
Rafael Miranda <r4free@gmail.com>
2022-05-13 00:19:45 -0300

    Registra rotas

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,18 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::group([
+    'prefix' => 'admin', 
+    'middleware' => 'auth',
+    'as' => 'admin.'
+], function(){
+    Route::get('/',[HomeController::class, 'index'])->name('home');
+});
 
+Route::get('/login', [LoginController::class, 'index'])->name('login');
+Route::get('/register', [RegisterController::class,'index']);
+Route::post('/register', [RegisterController::class,'store']);
