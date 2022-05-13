8fde071e8843225f58e4b3e1a4feb571bc1120b2
Rafael Miranda <r4free@gmail.com>
2022-05-13 09:59:57 -0300

    Add funcao criar gestor nos tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,20 @@
 
 namespace Tests;
 
+use App\Models\School;
+use App\Models\User;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    protected function createUserManager() : User
+    {
+        $school = School::factory()->create();
+        $user_data = User::factory()->make()->toArray();
+        $user_data['password'] = 123123;
+        $user = $school->addManager($user_data);
+        return $user;
+    }
 }
