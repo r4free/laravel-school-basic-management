1dd23c3712ff269357e677b2adb6d46c38c6a26e
Rafael Miranda <r4free@gmail.com>
2022-05-13 00:20:23 -0300

    Cria views das páginas iniciais

---
+++ resources/views/register/form.blade.php
@@ -0,0 +1,63 @@
+<form role="form" action="" method="POST">
+    @csrf
+    <div class="mb-3">
+        <input 
+        value="Escola teste"
+        type="text" 
+        class="form-control" 
+        placeholder="Nome da escola" 
+        aria-label="Nome da escola"
+        name="school_name"
+    >
+    </div>
+    <div class="mb-3">
+        <input 
+        value="Rua 4, 304 - passare"
+        type="text" 
+        class="form-control" 
+        placeholder="Endereço da escola" 
+        aria-label="Endereço da escola"
+        name="address"
+    >
+    </div>
+    <div class="mb-3">
+        <input 
+        value="rafael"
+        type="text" 
+        class="form-control" 
+        placeholder="Nome do usuário" 
+        aria-label=""
+        name="name"
+        >
+    </div>
+    <div class="mb-3">
+        <input 
+        value="rafael@mailhog.com"
+        type="email" 
+        class="form-control" 
+        placeholder="Email" 
+        aria-label="Email"
+        name="email"
+        >
+    </div>
+    <div class="mb-3">
+        <input
+        value="123123"
+        type="password" 
+        class="form-control" 
+        placeholder="Password" 
+        aria-label="Password"
+        name="password"
+        >
+    </div>
+    <div class="form-check form-check-info text-start">
+        <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" checked>
+        <label class="form-check-label" for="flexCheckDefault">
+        Concordo com os <a href="javascript:;" class="text-dark font-weight-bolder">Termos e condições</a>
+        </label>
+    </div>
+    <div class="text-center">
+        <button type="submit" class="btn bg-gradient-dark w-100 my-4 mb-2">Cadastrar</button>
+    </div>
+    <p class="text-sm mt-3 mb-0">Already have an account? <a href="javascript:;" class="text-dark font-weight-bolder">Sign in</a></p>
+</form>
---
+++ resources/views/register/index.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.sign')
+@section('content')
+<div class="page-header align-items-start min-vh-50 pt-5 pb-11 m-3 border-radius-lg" style="background-image: url('https://raw.githubusercontent.com/creativetimofficial/public-assets/master/argon-dashboard-pro/assets/img/signup-cover.jpg'); background-position: top;">
+      <span class="mask bg-gradient-dark opacity-6"></span>
+      <div class="container">
+        <div class="row justify-content-center">
+          <div class="col-lg-8 text-center mx-auto">
+            <h1 class="text-white mb-2 mt-5">Cadatro de escola!</h1>
+            <p class="text-lead text-white">Use these awesome forms to login or create new account in your project for free.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="container">
+      <div class="row mt-lg-n10 mt-md-n11 mt-n10 justify-content-center">
+        <div class="col-xl-8 mx-auto">
+          <div class="card z-index-0">
+            <div class="card-header text-center pt-4">
+              <h5>Register with</h5>
+            </div>
+            <div class="card-body">
+                @include('register.form')
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+@endsection
