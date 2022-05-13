cf40d91da62dcb505c44ac09547deacfa42660d2
Rafael Miranda <r4free@gmail.com>
2022-05-13 00:18:31 -0300

    Seeder de roles

---
+++ database/seeders/RolesSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Role;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class RolesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $roles = [
+            [
+                'id' => 1,
+                'name' => 'Aluno',
+            ],
+            [
+                "name" => 'gestor'
+            ]
+        ];
+
+        foreach($roles as $role){
+            Role::create($role);
+        }
+        
+    }
+}
