c4657334483b7d3ddadca3055b7717e1161e3190
Rafael Miranda <r4free@gmail.com>
2022-05-13 00:19:17 -0300

    Teste de cadastro de escola

--- tests/Feature/RegisterSchoolTest.php
+++ tests/Feature/RegisterSchoolTest.php
@@ -4,10 +4,12 @@ namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Http\Response;
 use Tests\TestCase;
 
 class RegisterSchoolTest extends TestCase
 {
+    use WithFaker, RefreshDatabase;
     /**
      * A basic feature test example.
      *
@@ -18,5 +20,76 @@ class RegisterSchoolTest extends TestCase
         $response = $this->get('/register');
 
         $response->assertStatus(200);
+    }
+    
+    public function test_user_can_register_school_and_login_after_create()
+    {
+        $data = [
+            'name' => $this->faker->name,
+            'school_name' => $this->faker->name,
+            'email' => $this->faker->email,
+            'password' => 123123
+        ];
+
+        $response = $this->post('/register', $data);
+
+        $response->assertStatus(302);
+    }
+    
+    public function test_email_is_required_on_register()
+    {
+        $data = [
+            'name' => $this->faker->name,
+            'school_name' => $this->faker->name,
+            'password' => 123123
+        ];
+
+        $response = $this->post('/register', $data);
+
+        $response
+        ->assertSessionHasErrors('email')
+        ->assertStatus(302);
+    }
+    
+    public function test_name_is_required_on_register()
+    {
+        $data = [
+            'school_name' => $this->faker->name,
+            'email' => $this->faker->email,
+            'password' => 123123
+        ];
+
+        $response = $this->post('/register', $data);
+
+        $response->assertSessionHasErrors('name')
+        ->assertStatus(302);
+    }
+    
+    public function test_school_name_is_required_on_register()
+    {
+        $data = [
+            'name' => $this->faker->email,
+            'email' => $this->faker->email,
+            'password' => 123123
+        ];
+
+        $response = $this->post('/register', $data);
+
+        $response->assertSessionHasErrors('school_name')
+        ->assertStatus(302);
+    }
+    
+    public function test_password_is_required_on_register()
+    {
+        $data = [
+            'school_name' => $this->faker->email,
+            'name' => $this->faker->email,
+            'email' => $this->faker->email,
+        ];
+
+        $response = $this->post('/register', $data);
+
+        $response->assertSessionHasErrors('password')
+        ->assertStatus(302);
     }
 }
