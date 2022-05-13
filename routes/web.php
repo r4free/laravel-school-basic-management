e9479329d3208fb1d15636db592c06f6fd57ea3f
Rafael Miranda <r4free@gmail.com>
2022-05-13 10:05:14 -0300

    Registrar rotas

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,10 @@
 <?php
 
+use App\Http\Controllers\GroupController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\StudentsController;
 use Illuminate\Support\Facades\Route;
 
 Route::group([
@@ -11,6 +13,19 @@ Route::group([
     'as' => 'admin.'
 ], function(){
     Route::get('/',[HomeController::class, 'index'])->name('home');
+
+    Route::group([
+        'prefix' => 'students',
+        'as' => 'students.'
+    ],function(){
+        Route::get('/',[StudentsController::class, 'index'])->name('index');
+    });
+    Route::group([
+        'prefix' => 'groups',
+        'as' => 'groups.'
+    ],function(){
+        Route::get('/',[GroupController::class, 'index'])->name('index');
+    });
 });
 
 Route::get('/login', [LoginController::class, 'index'])->name('login');
