2cbe5844b3d0ebba9068adf820f64adeef895536
Rafael Miranda <r4free@gmail.com>
2022-05-13 11:01:04 -0300

    factory gestor

---
+++ database/factories/ManagerFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Manager>
+ */
+class ManagerFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'role_id' => 2,
+            'name' => $this->faker->name(),
+            'email' => $this->faker->unique()->safeEmail(),
+            'email_verified_at' => now(),
+            'password' => 'password',
+            'remember_token' => \Str::random(10),
+        ];
+    }
+}
