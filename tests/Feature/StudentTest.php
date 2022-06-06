7555009cc4c8bc3c323d6c0b2cf24b2c5a0e4f98
Rafael Miranda <r4free@gmail.com>
2022-06-06 00:31:10 -0300

    Add test create student ajust test edit student

--- tests/Feature/StudentTest.php
+++ tests/Feature/StudentTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Models\Group;
 use App\Models\School;
 use App\Models\Student;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -10,7 +11,7 @@ use Tests\TestCase;
 
 class StudentTest extends TestCase
 {
-    use RefreshDatabase;
+    use RefreshDatabase, WithFaker;
     /**
      * A basic feature test example.
      *
@@ -21,7 +22,33 @@ class StudentTest extends TestCase
         $manager = $this->createUserManager(School::first());
         $response = $this->actingAs($manager)->get('admin/students');
         $response->assertStatus(200)
-        ->assertViewHas("students");
+        ->assertViewHas('students');
+    }
+
+
+
+    public function test_user_can_see_create_a_student_page()
+    {
+        $manager = $this->createUserManager(School::first());
+        $response = $this->actingAs($manager)
+        ->get(route('admin.students.create'));
+        $response->assertStatus(200)
+        ->assertViewHas("groups", Group::all());
+    
+    }
+   
+    public function test_user_can_create_a_student()
+    {
+        $manager = $this->createUserManager(School::first());
+        $response = $this->actingAs($manager)
+        ->post(route('admin.students.store'), [
+            "name" => $this->faker->name,
+            "email" => $this->faker->email,
+            "group_id" => Group::first()->id,
+        ]);
+        $response
+        ->assertRedirect(route('admin.students.index'));
+    
     }
 
     public function test_a_user_can_see_edit_student_page()
@@ -29,9 +56,13 @@ class StudentTest extends TestCase
         $manager = $this->createUserManager(School::first());
         $response = $this->actingAs($manager);
         $student = Student::first();
+        $groups = Group::all();
         $response = $response->get(route('admin.students.edit',  $student->id));
         $response->assertStatus(200)
-        ->assertViewHas("student", $student);
+        ->assertViewHas([
+            "student" => $student,
+            "groups" => $groups
+        ]);
     
     }
 
@@ -40,7 +71,7 @@ class StudentTest extends TestCase
         $manager = $this->createUserManager(School::first());
         $response = $this->actingAs($manager);
         $student = Student::first();
-        
+
         $response = $response->put(route('admin.students.update',  $student->id),[
             "name" => $student->name,
             "email" => $student->email,
