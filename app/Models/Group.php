ccf58ef3dfe74d6b09dc32a918a47a2aef620e44
Rafael Miranda <r4free@gmail.com>
2022-06-05 23:28:24 -0300

    add global scope to group accodint to current user school_id

--- app/Models/Group.php
+++ app/Models/Group.php
@@ -3,8 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\{Model, Builder};
 class Group extends Model
 {
     use HasFactory;
@@ -19,10 +18,20 @@ class Group extends Model
     public static function boot()
     {
         parent::boot();
+
+        static::addGlobalScope(function (Builder $builder)
+        {
+            $builder
+            ->when(\Auth::check(),function($builder) {
+                $builder->where('school_id', \Auth::user()->school_id);
+            });
+        });
+
         static::creating(function($model) {
             if(\Auth::check())
                 $model->school_id = \Auth::user()->school_id;
         });
+
     }
     
 }
