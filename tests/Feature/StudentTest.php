201db28dba58b0543a555a9acae991d9905ea907
Rafael Miranda <r4free@gmail.com>
2022-06-06 00:00:59 -0300

    Add tests edit student

--- tests/Feature/StudentTest.php
+++ tests/Feature/StudentTest.php
@@ -2,12 +2,15 @@
 
 namespace Tests\Feature;
 
+use App\Models\School;
+use App\Models\Student;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class StudentTest extends TestCase
 {
+    use RefreshDatabase;
     /**
      * A basic feature test example.
      *
@@ -15,9 +18,37 @@ class StudentTest extends TestCase
      */
     public function test_list_students()
     {
-        $manager = $this->createUserManager();
+        $manager = $this->createUserManager(School::first());
         $response = $this->actingAs($manager)->get('admin/students');
+        $response->assertStatus(200)
+        ->assertViewHas("students");
+    }
 
-        $response->assertStatus(200);
+    public function test_a_user_can_see_edit_student_page()
+    {
+        $manager = $this->createUserManager(School::first());
+        $response = $this->actingAs($manager);
+        $student = Student::first();
+        $response = $response->get(route('admin.students.edit',  $student->id));
+        $response->assertStatus(200)
+        ->assertViewHas("student", $student);
+    
+    }
+
+    public function test_a_user_can_see_update_student()
+    {
+        $manager = $this->createUserManager(School::first());
+        $response = $this->actingAs($manager);
+        $student = Student::first();
+        
+        $response = $response->put(route('admin.students.update',  $student->id),[
+            "name" => $student->name,
+            "email" => $student->email,
+            "group_id" => $student->group_id,
+        ]);
+
+        $response->assertRedirect(route('admin.students.index'));
+    
     }
 }
+
