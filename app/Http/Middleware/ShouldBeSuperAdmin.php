d62a5f87b67d3d4e7fda122685e0cfd466f689db
Rafael Miranda <r4free@gmail.com>
2022-06-06 22:20:00 -0300

    Create superadmin middleware

---
+++ app/Http/Middleware/ShouldBeSuperAdmin.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class ShouldBeSuperAdmin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        abort_unless(\Auth::user()->is_super_admin, 403);
+        return $next($request);
+    }
+}
