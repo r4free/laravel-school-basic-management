2d6314b834a354230770fcdd406db538dad21e85
Rafael Miranda <r4free@gmail.com>
2022-06-06 22:33:26 -0300

    Filter school according to role_id

--- app/Models/School.php
+++ app/Models/School.php
@@ -3,13 +3,25 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\{Model, Builder};
 
 class School extends Model
 {
     use HasFactory;
 
     protected $fillable = ['name','address'];
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::addGlobalScope(function(Builder $builder){
+            $builder->when(\Auth::check() && !\Auth::user()->is_super_admin, function($query){
+                $query->forCurrentUser();
+            });
+        });
+
+    }
 
     public function users()
     {
@@ -20,6 +32,11 @@ class School extends Model
     {
         $data['role_id'] = 2;
         return $this->users()->create($data);
+    }
+
+    public function scopeForCurrentUser($query)
+    {
+        $query->where('id', \Auth::user()->school_id);
     }
 
 }
