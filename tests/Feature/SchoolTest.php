4dcb240b8cf23e6c53291c62c35c02fafc845edc
Rafael Miranda <r4free@gmail.com>
2022-06-06 21:43:23 -0300

    test a user can see edit school page

--- tests/Feature/SchoolTest.php
+++ tests/Feature/SchoolTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Models\School;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -16,6 +17,17 @@ class SchoolTest extends TestCase
         $response = $this->actingAs($superadmin)->get(route('admin.schools.index'));
         $response->assertStatus(200)
         ->assertViewHas('schools');
+    }
+
+    public function test_a_user_can_see_edit_school_page()
+    {
+        $superadmin = $this->createSuperAdmin();
+        $response = $this->actingAs($superadmin);
+        $school = School::first();
+        $response = $response->get(route('admin.schools.edit',  $school->id));
+        $response->assertStatus(200)
+        ->assertViewHas("school", $school);
+    
     }
     
 }
